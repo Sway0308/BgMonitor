d863fdab6cf87fe316728642e15708763177337b
sway <sway_chen@mayohr.com>
2019-08-08 10:08:43 +0800

    加入機碼監視器

---
+++ RegistryMonitor/MainForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace RegistryMonitor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnSetTimer = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.BackToNormal = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.CloseForm = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSetTimer
+            // 
+            this.btnSetTimer.Location = new System.Drawing.Point(177, 161);
+            this.btnSetTimer.Name = "btnSetTimer";
+            this.btnSetTimer.Size = new System.Drawing.Size(75, 23);
+            this.btnSetTimer.TabIndex = 0;
+            this.btnSetTimer.Text = "停止計時器";
+            this.btnSetTimer.UseVisualStyleBackColor = true;
+            this.btnSetTimer.Click += new System.EventHandler(this.btnSetTimer_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BackToNormal,
+            this.CloseForm});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(135, 48);
+            this.contextMenuStrip1.Text = "背景監視器";
+            // 
+            // BackToNormal
+            // 
+            this.BackToNormal.Name = "BackToNormal";
+            this.BackToNormal.Size = new System.Drawing.Size(134, 22);
+            this.BackToNormal.Text = "回覆原視窗";
+            this.BackToNormal.Click += new System.EventHandler(this.BackToNormal_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.richTextBox1.ForeColor = System.Drawing.Color.Chartreuse;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(264, 155);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 3000;
+            this.timer.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // CloseForm
+            // 
+            this.CloseForm.Name = "CloseForm";
+            this.CloseForm.Size = new System.Drawing.Size(134, 22);
+            this.CloseForm.Text = "關閉監視器";
+            this.CloseForm.Click += new System.EventHandler(this.FormClose_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 196);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnSetTimer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "背景監視器";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSetTimer;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem BackToNormal;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ToolStripMenuItem CloseForm;
+    }
+}
+
---
+++ RegistryMonitor/MainForm.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistryMonitor
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            WriteText("Start timer", false);
+            timer.Start();
+        }
+
+        private void BackToNormal_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+            this.notifyIcon1.Visible = false;
+        }
+
+        private void DeleteRegistryKey()
+        {
+            //讀取Registry Key位置
+            using (var RegK = Registry.LocalMachine.OpenSubKey(@"Software\Policies\Google\Chrome", RegistryKeyPermissionCheck.ReadWriteSubTree))
+            {
+                var value = RegK.GetValue("RemoteAccessHostFirewallTraversal")?.ToString();
+                if (value != null)
+                {
+                    WriteText($"Registry Value = {value}");
+                    WriteText($"Start delete registry");
+                    RegK.DeleteValue("RemoteAccessHostFirewallTraversal", true);
+                    WriteText($"finish delete registry");
+                }
+                else
+                {
+                    WriteText("No Registry");
+                }
+            }
+        }
+
+        private void WriteText(string text, bool newLine = true)
+        {
+            if (newLine)
+                richTextBox1.AppendText(Environment.NewLine);
+            richTextBox1.AppendText(text);
+            richTextBox1.SelectionStart = richTextBox1.TextLength;
+            richTextBox1.ScrollToCaret();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            timer.Interval = 10 * 60 * 1000; //每10分鐘檢查一次
+            WriteText(DateTime.Now.ToString());
+            DeleteRegistryKey();
+            Console.ReadLine();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                var result = MessageBox.Show("是否縮小到工具列", "確認視窗", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    this.WindowState = FormWindowState.Minimized;        //決定視窗大小
+                    this.ShowInTaskbar = false;                          //決定是否出現在工作列
+                    this.notifyIcon1.Visible = true;                     //決定使否顯示notifyIcon1
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void FormClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSetTimer_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = !timer.Enabled;
+            btnSetTimer.Text = timer.Enabled ? "停止計時器" : "啟動計時器";
+        }
+    }
+}

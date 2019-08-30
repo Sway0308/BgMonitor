fe271c1d37422a113771036bc3f00e2404a1dab2
sway <sway_chen@mayohr.com>
2019-08-30 10:50:30 +0800

    加入異常處理機制 應用程式改為64位元

--- RegistryMonitor/Program.cs
+++ RegistryMonitor/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +16,86 @@ namespace RegistryMonitor
         [STAThread]
         static void Main()
         {
+            HandleException();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+        private static void HandleException()
+        {
+            // Add the event handler for handling UI thread exceptions to the event.
+            Application.ThreadException += new ThreadExceptionEventHandler(UIThreadException);
+
+            // Set the unhandled exception mode to force all Windows Forms errors to go through our handler.
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+
+            // Add the event handler for handling non-UI thread exceptions to the event. 
+            AppDomain.CurrentDomain.UnhandledException +=
+                new UnhandledExceptionEventHandler(UnhandledException);
+        }
+
+        /// <summary>
+        /// UI thread exceptions 處理函式。
+        /// </summary>
+        private static void UIThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            try
+            {
+                //顯示例外錯誤訊息視窗
+                MessageBox.Show("Fatal UI Error", e.Exception.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch
+            {
+                try
+                {
+                    MessageBox.Show("Fatal Windows Forms Error",
+                        "Fatal Windows Forms Error", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Stop);
+                }
+                finally
+                {
+                    Application.Exit();
+                }
+            }
+        }
+
+        /// <summary>
+        /// non-UI thread exceptions 處理函式。
+        /// </summary>
+        private static void UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                Exception ex = (Exception)e.ExceptionObject;
+                string errorMsg = "An application error occurred. Please contact the adminstrator " +
+                    "with the following information:\n\n";
+
+                // Since we can't prevent the app from terminating, log this to the event log. 
+                if (!EventLog.SourceExists("ThreadException"))
+                {
+                    EventLog.CreateEventSource("ThreadException", "Application");
+                }
+
+                // Create an EventLog instance and assign its source.
+                EventLog myLog = new EventLog
+                {
+                    Source = "ThreadException"
+                };
+                myLog.WriteEntry(errorMsg + ex.Message + "\n\nStack Trace:\n" + ex.StackTrace);
+            }
+            catch (Exception exc)
+            {
+                try
+                {
+                    MessageBox.Show("Fatal Non-UI Error",
+                        "Fatal Non-UI Error. Could not write the error to the event log. Reason: "
+                        + exc.Message, MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                }
+                finally
+                {
+                    Application.Exit();
+                }
+            }
         }
     }
 }
